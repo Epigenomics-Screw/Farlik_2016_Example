5c3edd9f1e371501d524c5f771b2cbdf81dd2eaf
koneill <koneill@bcgsc.ca>
2017-07-22 10:23:27 +0200

    Code probably working

---
+++ run_farlik_pipeline.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+files=data/*
+
+for i in $files
+do
+  cwltool  Screw/cwl/tools/preprocess.cwl --toConvert $i --format "farlik2016"
+done
+
+mkdir meth meth_sym prop_meth cov_bw subset
+
+mv *.meth meth
+mv *meth.sym meth_sym
+mv *prop_meth.bw prop_meth
+mv *.cov.bw cov_bw
+
+
+files=meth_sym/*
+for i in $files
+do
+  cwltool Screw/cwl/tools/subsetByBed.cwl --bedFile hg19_enhancer_atlas_cd34.bed --toSubset $i
+done
+
+mv *meth.sym subset
+
+cwltool Screw/cwl/tools/clustering.cwl --pairDirectory subset --annotation heatmap.csv 
+
+
+
